f8e23b79c7b9fbcbb5d11307b20e2e0550793005
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-27 18:49:59 +0100

    Config express

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-console.log('It works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'It works!'} );
+});
+
+app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
