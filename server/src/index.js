de766f466471a5d7c8a4ae7633634e8b954a0533
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-27 21:42:49 +0100

    Add /temp and /data/catalog endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,15 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'It works!'} );
+    res.redirect('/temp');
+});
+
+app.get('/temp', (req, res) => {
+    res.json({ temp: true });
+});
+
+app.get('/data/catalog', (req, res) => {
+    res.json({});
 });
 
 app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
