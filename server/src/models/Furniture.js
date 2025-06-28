e883f1a52a18382183731301ab9a9cfb15609148
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-28 19:51:24 +0100

    Add furniture validations

--- server/src/models/Furniture.js
+++ server/src/models/Furniture.js
@@ -3,27 +3,34 @@ import { Schema, model } from "mongoose";
 const furnitureSchema = new Schema({
     make: {
         type: String,
-        required: true,
+        required: [true, 'Make is required'],
+        minLength: [4, 'Make must be at least 4 characters long'],
     },
     model: {
         type: String,
-        required: true,
+        required: [true, 'Model is required'],
+        minLength: [4, 'Model must be at least 4 characters long'],
     },
     year: {
         type: Number,
-        required: true,
+        required: [true, 'Year is required'],
+        min: [1950, 'Year must be 1950 or later'],
+        max: [2050, 'Year must be 2050 or earlier'],
     },    
     description: {
         type: String,
-        required: true,
+        required: [true, 'Description is required'],
+        minLength: [10, 'Description must be at least 10 characters long'],
     },
     price: {
         type: Number,
-        required: true,
+        required: [true, 'Price is required'],
+        min: [0.01, 'Price must be a positive number'],
     },
     img: {
         type: String,
-        required: true,
+        required: [true, 'Image is required'],
+        validate: [/^https?:\/\//, 'Invalid image URL'],
     },
     material: {
         type: String,
