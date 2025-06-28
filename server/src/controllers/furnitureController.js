af5dc456d2e5c845bb85ec9ccb330d8f93d1092b
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-28 16:03:28 +0100

    Add furniture controller

---
+++ server/src/controllers/furnitureController.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const furnitureController = Router();
+
+furnitureController.post('/', async (req, res) => {
+    const furnitureData = req.body;
+
+    console.log(furnitureData);
+
+    res.end();
+});
+
+export default furnitureController;
