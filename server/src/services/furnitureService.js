9d855070cfa0ae79c785a3a5e5469fb0b65acd2f
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-28 19:57:01 +0100

    Add furniture details page

--- server/src/services/furnitureService.js
+++ server/src/services/furnitureService.js
@@ -1,10 +1,13 @@
 import Furniture from "../models/Furniture.js"
 
 export default {
+    getAll() {
+        return Furniture.find();
+    },
+    getOne(furnitureId) {
+        return Furniture.findById(furnitureId);
+    },
     create(furnitureData) {
         return Furniture.create(furnitureData);
     },
-    getAll() {
-        return Furniture.find();
-    }
 }
